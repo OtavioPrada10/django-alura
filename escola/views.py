07270b0a400fb4584d054ef818f1a48e2816f86c
Otavio Prada <otaviovinicius07@hotmail.com>
2024-03-04 17:59:06 -0300

    update

--- escola/views.py
+++ escola/views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
 from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaAlunosMatriculadosSerializer, ListaMatriculasAlunoSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
